fa80128897c9e222bf369c7006f0ab46534cfaba
Paula Boyano <paulaboyanoivars@gmail.com>
2022-05-18 13:00:10 +0200

    day1 project

---
+++ day1/band_name_generator.py
@@ -0,0 +1,7 @@
+print("Hello! This is a band name generator. :)")
+
+city = input("In which city you grew up in? ")
+
+pet = input("What is the name of your pet? ")
+
+print("Your band name is: " + city + " " + pet + "!")
