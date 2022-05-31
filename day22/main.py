d85661c5b02f4bac2ae6c34c39d713d7b9267dd6
waleska404 <paulaboyanoivars@gmail.com>
2022-05-31 13:44:11 +0200

    day22: basc pong

--- day22/main.py
+++ day22/main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.bgcolor("pink")
@@ -10,6 +13,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -21,6 +26,30 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    # detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # R paddle fails
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # L paddle fails
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+    # R paddle boundaries
+
+    # L paddle boundaries
 
 screen.exitonclick()
